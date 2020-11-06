4f76640872f4cc23b2124b00981b7967e9ea1ff2
Mateusz Lewtak <lewtakm@gmail.com>
2020-11-06 15:16:24 +0100

    Feat: plugin init

---
+++ src/api/index.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 RoadieHQ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createApiRef, DiscoveryApi } from '@backstage/core';
+
+export const buildKiteApiRef = createApiRef<JiraAPI>({
+  id: 'plugin.jira.service',
+  description: 'Used by the Jira plugin to make requests',
+});
+
+const DEFAULT_PROXY_PATH = '/jira/api/';
+
+type Options = {
+  discoveryApi: DiscoveryApi;
+  /**
+   * Path to use for requests via the proxy, defaults to /buildkite/api
+   */
+  proxyPath?: string;
+};
+
+export class JiraAPI {
+  private readonly discoveryApi: DiscoveryApi;
+  private readonly proxyPath: string;
+
+  constructor(options: Options) {
+    this.discoveryApi = options.discoveryApi;
+    this.proxyPath = options.proxyPath ?? DEFAULT_PROXY_PATH;
+  }
+
+  private async getApiUrl() {
+    const proxyUrl = await this.discoveryApi.getBaseUrl('proxy');
+    return `${proxyUrl}${this.proxyPath}`;
+  }
+}
